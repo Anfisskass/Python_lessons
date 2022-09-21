114553df83a83bb7a38fae93c201e33bc0f778cd
anfisskass <nasyazh@mail.ru>
2022-09-21 23:23:16 +0300

    Add lecture_2 pract

---
+++ lecture_2_practika.py
@@ -0,0 +1,27 @@
+#colors = ['red', 'green', 'blue']
+#data = open('file.txt', 'a')
+#data.writelines(colors)
+#data.close
+
+#with open('file.txt', 'w') as data:
+#    data.write('line 1\n')
+#    data.write('line 2\n') #закрытие файла
+
+
+#path = 'file.txt'
+#data = open(path, 'r')
+#for line in data:
+#    print(line)
+#data.close()
+
+#import lecture_1
+
+#print(lecture_1.f())
+
+def concatenatio(*params):
+        res: str = ""
+        for item in params:
+            res += item
+        retur res
+print(concatenatio)
+
