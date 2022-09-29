999cfc53ea86bbc801ee1401666be63323e79a3b
anfisskass <nasyazh@mail.ru>
2022-09-30 00:37:55 +0300

    Add lecture_4

---
+++ lecture_4.py
@@ -0,0 +1,10 @@
+x = int(input('x = '))
+y = int(input('y = '))
+print(f'{x} + {y} = {x + y}')
+
+def sum(a, b):
+ return a + b
+x = int(input('x = '))
+y = int(input('y = '))
+print(f'{x} + {y} = {sum(x, y)}')
+
